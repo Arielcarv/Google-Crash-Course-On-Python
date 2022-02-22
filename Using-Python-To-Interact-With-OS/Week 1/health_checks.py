af852b39058921e66969b479cf0b439f2ddbd70f
Ariel Nascimento <ariel.nascimento@b2wdigital.com>
2022-02-22 15:32:51 -0300

    refactor:  Week1 health-check

--- Using-Python-To-Interact-With-OS/Week 1/health_checks.py
+++ Using-Python-To-Interact-With-OS/Week 1/health_checks.py
@@ -6,14 +6,17 @@ def check_disk_usage(disk):
     '''Returns True if the disk usage is less than 80%'''
     disk_usage = shutil.disk_usage(disk)
     free_space = (disk_usage.free / disk_usage.total) * 100
+    print(f"Disk Usage: {disk_usage}.")
     return free_space > 20
 
 def check_cpu_usage():
     '''Returns True if CPU usage is less than 75%'''
-    usage = psutil.cpu_percent(1)
-    return usage < 75 
+    cpu_usage = psutil.cpu_percent(1)
+    print(f"CPU Usage: {cpu_usage}")
+    return cpu_usage < 75 
 
-if not check_disk_usage("/") or not check_cpu_usage():
-    print("ERROR")
+if check_disk_usage("/") or check_cpu_usage():
+    print("Everything is OK!") 
 else:
-    print("Everything is OK!")
+    print("ERROR")
+    
