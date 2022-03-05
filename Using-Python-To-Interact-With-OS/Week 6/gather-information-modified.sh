62597dcdcb99189879dfa738b94cd470940ff48f
Ariel Carvalho <arielcarvnasc@gmail.com>
2022-03-04 21:59:15 -0300

    feat : gather-information script

---
+++ Using-Python-To-Interact-With-OS/Week 6/gather-information-modified.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Starting at: $(date)"
+
+echo -e "\nUPTIME"; uptime
+
+echo -e "\nFREE"; free
+
+echo -e "\nWHO"; who
+
+echo -e "\nFinishing at: $(date)"
---
+++ Using-Python-To-Interact-With-OS/Week 6/gather-information.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "Starting at: $(date)\n"
+
+echo "UPTIME"
+uptime
+echo
+
+echo "FREE"
+free
+echo
+
+echo "WHO"
+who
+echo
+
+echo "Finishing at: $(date)"
